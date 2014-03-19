fcc1b3cfa0ac58df7ffaafbf718020a7f61a8ab9
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2014-03-19 23:35:21 +0100

    Use only one timer

--- src/com/example/whocares/counter/MainActivity.java
+++ src/com/example/whocares/counter/MainActivity.java
@@ -9,30 +9,48 @@ import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	Timer foregroundTimer = new Timer();
-	Timer totalTimer = new Timer();
+	Timer timer;
+	long startTime;
+	long resumeTime;
+	long previousForegroundTime;
+	
+	private long getTime() {
+		return System.currentTimeMillis();
+	}
 	
 	@Override
 	protected void onPause() {
 		super.onPause();
-		foregroundTimer.cancel();
+		timer.cancel();
+		previousForegroundTime += getTime() - resumeTime;
 	}
 	
-	int foreground;
-	int total;
+	private void updateStuff() {
+		long currentTime = getTime();
+		
+		TextView totalTimeView = (TextView) findViewById(R.id.total_time);
+		totalTimeView.setText("total: " + (currentTime - startTime));
+		
+		long currentForegroundTime = currentTime - resumeTime;
+		TextView foregroundTimeView = (TextView) findViewById(R.id.foreground_time);
+		foregroundTimeView.setText("current foreground: " + currentForegroundTime);
+		
+		TextView currentforegroundTimeView = (TextView) findViewById(R.id.current_foreground_time);
+		currentforegroundTimeView.setText("total foreground: " + (currentForegroundTime + previousForegroundTime));
+	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
-		foregroundTimer = new Timer();
-		foregroundTimer.schedule(new TimerTask() {
+		resumeTime = getTime();
+		timer = new Timer();
+		timer.schedule(new TimerTask() {
 			@Override
 			public void run() {
 				runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
-						TextView textView = (TextView) findViewById(R.id.foreground_time);
-						textView.setText("" + ++foreground);
+						updateStuff();
 					}
 				});
 			}
@@ -44,19 +62,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		totalTimer.schedule(new TimerTask() {
-			@Override
-			public void run() {
-				runOnUiThread(new Runnable(){
-					@Override
-					public void run() {
-						TextView textView = (TextView) findViewById(R.id.total_time);
-						textView.setText("" + ++total);
-					}
-				});
-				
-			}
-		}, 0, 1000);
+		startTime = getTime();
 	}
 
 	@Override
